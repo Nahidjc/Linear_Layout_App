d7035a53dc639dd94693d2c3b6ce2abe270524ff
Nahid2194 <nahidjc80@gmail.com>
2021-01-31 21:52:01 +0600

    store data in variable

--- app/src/main/java/com/example/linear_layout/MainActivity.java
+++ app/src/main/java/com/example/linear_layout/MainActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+  EditText nameId,phoneId;
+  TextView displayTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameId = findViewById(R.id.nameId);
+        phoneId = findViewById(R.id.phoneId);
+        displayTextView = findViewById(R.id.displayTextView);
     }
 
     public void displayFunction(View view) {
